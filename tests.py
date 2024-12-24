e00bcb2e8adf86f1e01b0cfc433b8532c15ce7c4
rodrigo barbosa <rodrigo019barbosa@gmail.com>
2024-12-24 08:46:48 -0300

    first commit

---
+++ tests.py
@@ -0,0 +1,15 @@
+
+import time
+
+def contagem_regressiva(segundos):
+    while segundos:
+        mins, secs = divmod(segundos, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        segundos -= 1
+
+    print("Tempo esgotado!")
+
+# Exemplo de uso
+contagem_regressiva(10)  # Contagem regressiva de 10 segundos
